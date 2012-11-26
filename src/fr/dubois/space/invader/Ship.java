477506081a95b65d440dac91080cf69ba6c75a6a
gwenaelle.damiens <gwenaelle.damiens@103F16.etu.iut-lens.univ-artois.fr>
2012-11-26 11:27:32 +0100

    Ajout du vaisseau

---
+++ src/fr/dubois/space/invader/Ship.java
@@ -0,0 +1,18 @@
+package fr.dubois.space.invader;
+
+import android.graphics.Bitmap;
+
+public class Ship extends Sprite{
+
+	public Ship(Bitmap bitmap, float x, float y) {
+		super(bitmap, x, y);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void act() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
